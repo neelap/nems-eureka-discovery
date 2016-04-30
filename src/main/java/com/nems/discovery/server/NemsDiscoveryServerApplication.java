badef7d72ce6687208f4badc519cb90b91f48f40
neelap <neela_reddy@outlook.com>
2016-04-30 18:49:25 +0530

    dgdg

--- src/main/java/com/nems/discovery/server/NemsDiscoveryServerApplication.java
+++ src/main/java/com/nems/discovery/server/NemsDiscoveryServerApplication.java
@@ -1,14 +1,10 @@
 package com.nems.discovery.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@ComponentScan
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableEurekaServer
 public class NemsDiscoveryServerApplication {
 
